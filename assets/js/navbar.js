8becac8a22989a1f6540d348edd2db57e7a7130d
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-03-14 08:20:37 +0200

    Add files via upload

---
+++ assets/js/navbar.js
@@ -0,0 +1,36 @@
+/*!  
+ * Mini projektas v1 (http://manowebas.lt/)
+ * Copyright 2023 The  Authors Dariaus Jakaičio projektas
+ * Licensed under MIT (https://github.com/gripas/responsive-menu/blob/main/LICENSE)
+ */
+
+$(document).ready(function() {
+	
+    $('.nav-toggle').click(function(){
+      var collapse = $(this).attr('data-collapse');					
+        let nMenu = function menu(){
+        if($(this).css('display')=='none'){      
+          t_change.html('<i class="fa fa-bars"></i>');
+        }else{
+          t_change.html('<i class="fa fa-bars"></i>');
+        }
+      }
+      var t_change = $(this);
+      $(collapse).toggle(nMenu);
+    });
+          
+ 
+    $('.v2-toggle').click(function(){
+      var collapse = $(this).attr('data-collapse_2');					
+        let nMenu = function menu(){
+        if($(this).css('display')=='none'){      
+          t_change.html('<i class="fa fa-bars"></i>');
+        }else{
+          t_change.html('<i class="fa fa-bars"></i>');
+        }
+      }
+      var t_change = $(this);
+      $(collapse).toggle(nMenu);
+    });
+          
+  });	
